36036fc7d9f7efec36544ee8cfacb632b581a0db
Skr0ut <mascini.max@dal.ca>
2021-11-25 12:52:33 -0400

    Update

--- Data MEANS - Graphs.py
+++ Data MEANS - Graphs.py
@@ -4,7 +4,10 @@ import pandas as pd
 import matplotlib.pyplot as plt
 from scipy.stats import ttest_ind
 
-dfM = pd.read_csv('https://raw.githubusercontent.com/Skr0ut/3131-3505-Data/main/3131%20Data%20MEANS.csv')
+dfM = pd.read_csv('https://raw.githubusercontent.com/Skr0ut/3131-3505-Data/main/Data/3131%20Data%20MEANS.csv')
 print(dfM.sample(15))
 
 
+sns.relplot(data=dfM, x='rt', y='acc', col='flanker', hue='bias', kind='line', palette='Paired')
+
+plt.show()

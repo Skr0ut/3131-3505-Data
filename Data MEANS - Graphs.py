ffdf928fc6bef594f1ddb866059461819b2bd99a
Skr0ut <mascini.max@dal.ca>
2021-11-25 12:30:16 -0400

    Added Means data + means graphing

---
+++ Data MEANS - Graphs.py
@@ -0,0 +1,8 @@
+import numpy as np
+import seaborn as sns
+import pandas as pd
+import matplotlib.pyplot as plt
+from scipy.stats import ttest_ind
+
+dfM = pd.read_csv('https://raw.githubusercontent.com/Skr0ut/Portfolio-1/main/Data.csv?token=AWRQD5K6OVZNAE5XWO6EKBTBUD5NI')
+
